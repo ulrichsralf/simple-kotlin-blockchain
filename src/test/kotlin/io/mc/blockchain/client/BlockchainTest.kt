0abc9f902867cb06f789739837fcd6c6d962b93e
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2017-10-02 01:49:28 +0200

    added test

---
+++ src/test/kotlin/io/mc/blockchain/client/BlockchainTest.kt
@@ -0,0 +1,31 @@
+package io.mc.blockchain.client
+
+import org.junit.Test
+import java.nio.file.Paths
+
+/**
+ * @author Ralf Ulrich
+ * 31.08.17
+ */
+class BlockchainTest {
+
+
+    val client = BlockchainClient("http://vpf.mind-score.de:8080")
+
+    @Test
+    fun testAddAddress() {
+        client.generateKeyPair()
+        client.publishAddress(client.generateAddress( Paths.get("key.pub")))
+    }
+
+    @Test
+    fun testAddTransaction(){
+        client.generateKeyPair()
+        val address = client.generateAddress(Paths.get("key.pub"))
+        client.publishAddress(address)
+        val transaction = client.generateTransaction(Paths.get("key.priv"), "Hello Blockchain", address.id!!)
+        client.publishTransaction(transaction)
+
+    }
+
+}
